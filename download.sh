bb223220ad4fd40ebad2e767d5ddaa88f1fb3f7c
aaoganov <3145tttt002@mail.ru>
2025-11-04 11:36:54 +0300

    update download

--- download.sh
+++ download.sh
@@ -1,9 +1,8 @@
 mkdir ./data
 wget https://downloads.wortschatz-leipzig.de/corpora/hye_wikipedia_2021_1M.tar.gz -O ./data/wiki_2021.tar.gz
-
 tar -xvzf ./data/wiki_2021.tar.gz -C ./data
 rm ./data/wiki_2021.tar.gz
 
-wget https://downloads.wortschatz-leipzig.de/corpora/hye_community_2017.tar.gz -O ./data/community_2017.tar.gz
-tar -xvzf ./data/community_2017.tar.gz  -C ./data
-rm ./data/community_2017.tar.gz
+# wget https://downloads.wortschatz-leipzig.de/corpora/hye_community_2017.tar.gz -O ./data/community_2017.tar.gz
+# tar -xvzf ./data/community_2017.tar.gz  -C ./data
+# rm ./data/community_2017.tar.gz
